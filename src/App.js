2ac8f4e6ea5986c85a13de54b5b7a98dcf6c37e7
Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
2020-06-19 12:56:08 -0400

    home page complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+
 class App extends Component {
   
   render() {
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,17 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../Aux';
 import NavBar from '../../components/NavBar/NavBar';
 import Header from '../../components/Header/Header';
+import Particles from 'react-particles-js';
+import{makeStyles} from '@material-ui/core/styles'
 
-class Layout extends Component {
-    render() {
-        return (
-            <Aux>
-                <NavBar />
-                <Header />
-            </Aux>
-        );
+//CSS Styles
+const useStyles = makeStyles({
+    particlesCanva:{
+        position: "absolute",
+        opacity: "0.3"
     }
+});
+
+const Layout = () => {
+
+    const classes = useStyles();
+
+    return (
+        <Aux>
+            <NavBar />
+            <Header />
+            <Particles 
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles:{
+                        number:{
+                            value: 50,
+                            density: {
+                                enable: true,
+                                value_area: 1000
+                            }
+                        },
+                        size: {
+                            value: 3,
+                        },
+                    },
+                    interactivity:{
+                        events:{
+                            onHover:{
+                                enable: true,
+                                mode: "repulse"
+                            }
+                        }
+                    }
+                }}/>
+        </Aux>
+    );
 }
 
 export default Layout;
