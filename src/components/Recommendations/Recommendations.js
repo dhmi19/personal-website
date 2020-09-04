d83ab4a12fed74614ad4c87fb328292e791d65d0
Dhruv Mittal <d2mittal@edu.uwaterloo.ca>
2020-09-04 18:38:05 -0400

    fixed recommendations

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -13,13 +13,13 @@ const Recommendations = () => {
             position: "CEO, Polar",
             imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQEqMdYwewicFg/profile-displayphoto-shrink_800_800/0?e=1604534400&v=beta&t=r7ejs1ewiuQeWa23pVDvVDA0-LSeQ1UyacgkCmpv_YY",
             description: "Dhruv is a smart, resourceful and talented professional. He brings a curiosity to every project, willing to dive in and learn along the way. I counted on him for a variety of responsibilities and have high hopes for his future endeavours.",
-        },
+        },/*
         {
             name: "Omar Gupta",
             position: "Teammate, Polar",
             imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQEqMdYwewicFg/profile-displayphoto-shrink_800_800/0?e=1604534400&v=beta&t=r7ejs1ewiuQeWa23pVDvVDA0-LSeQ1UyacgkCmpv_YY",
             description: "Dhruv is a smart, resourceful and talented professional. He brings a curiosity to every project, willing to dive in and learn along the way. I counted on him for a variety of responsibilities and have high hopes for his future endeavours.",
-        }
+        }*/
     ]
 
     const settings = {
